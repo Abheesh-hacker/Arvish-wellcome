b15d17b9a32bf448f85d09b8f3da560d3e93d37d
Arvish <147795238+Abheesh-hacker@users.noreply.github.com>
2024-02-26 17:32:51 +0530

    Update update.sh
    Signed-off-by: Arvish  <147795238+Abheesh-hacker@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Update Script for Termux_welcome v2.1-Stable
-# Script created by @certified_youtuber
+# Script created by @arvish
 
 
 dependencies() {
@@ -16,7 +16,7 @@ printf "\n \e[1;92mUpdating \e[1;94mTERMUX-WELCOME\e[1;92m ...\n\n"
 sleep 1.5
 cd ..
 rm -rf TERMUX-WELCOME
-git clone https://github.com/itzR00TBYTE/TERMUX-WELCOME
+git clone https://github.com/Abheesh-hacker/Arvish-wellcome/
 cd TERMUX-WELCOME
 printf "\n\e[1;92mRestarting ...\n\e[0m"
 bash install.sh
